be9d0c7552be53550a6cfaf7812907de4b8db083
Foy Alexis <bwb.foy.a@fondespierre.com>
2025-12-01 09:57:06 +0100

    Réinitialisation du projet afin de le reprendre en l'adaptant à l'architecture MVC

--- Src/Interfaces/JobInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Src\Interfaces;
-interface JobInterface {
-public function getName(): string;
-public function getDescription(): string;
-public function getAllowedWeapons(): array;
-public function getAllowedSkills(): array;
-}
